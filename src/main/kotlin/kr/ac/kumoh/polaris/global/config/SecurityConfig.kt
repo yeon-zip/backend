7e3df69033d45b2559c8bc46ce81f95c9079f16e
PARK, Yeonjong <yeonjong.park@outlook.com>
2026-04-25 21:00:28 +0900

    feat: CORS 설정이 반영되지 않는 문제 수정

--- src/main/kotlin/kr/ac/kumoh/polaris/global/config/SecurityConfig.kt
+++ src/main/kotlin/kr/ac/kumoh/polaris/global/config/SecurityConfig.kt
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfigurationSource
 import tools.jackson.databind.ObjectMapper
 
 @Configuration
@@ -27,6 +28,7 @@ import tools.jackson.databind.ObjectMapper
 @EnableMethodSecurity
 class SecurityConfig(
     private val objectMapper: ObjectMapper,
+    private val corsConfigurationSource: CorsConfigurationSource,
     private val jwtTokenProvider: JwtTokenProvider,
     private val kakaoOidcUserService: KakaoOidcUserService,
     private val oauth2AuthenticationSuccessHandler: OAuth2AuthenticationSuccessHandler,
@@ -45,7 +47,9 @@ class SecurityConfig(
             csrf { disable() }
             httpBasic { disable() }
             formLogin { disable() }
-            cors { CorsConfig() }
+            cors {
+                configurationSource = corsConfigurationSource
+            }
             authorizeHttpRequests {
                 authorize("/api/v1/auth/kakao/login", permitAll)
                 authorize("/api/v1/auth/exchange", permitAll)
